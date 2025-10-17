ba2befd86ef127f9024196db1bcb98cc4fc00c99
MateusMartinsViana <mateusmartinsvianapc@gmail.com>
2025-10-17 20:23:00 -0300

    VerificadorVoto

---
+++ src/atividades/Main.java
@@ -0,0 +1,34 @@
+package atividades;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int opcao, idade;
+        
+    Scanner sc = new Scanner(System.in);
+    
+    do {
+        System.out.println("1-Verificador de idade para votacao");
+        System.out.println("2-Calculadora de area de quadrado");
+        System.out.println("3-Conversor de temperatura (celsius para fahrenheit)");
+        System.out.println("4-Concatenador de nome completo)");
+        System.out.println("5-Sair");
+        System.out.print("Escolha uma opcao: ");
+        opcao = sc.nextInt();
+        sc.nextLine(); // limpa o buffer
+        
+        switch (opcao){
+            case 1:
+                System.out.println("Digite sua idade: ");
+                idade = sc.nextInt();
+                
+                VerificadorVoto oVerificador = new VerificadorVoto(idade);
+                oVerificador.podeVotar();
+                             
+        }
+        
+    } while (opcao != 5);
+        
+    }
+}
---
+++ src/atividades/VerificadorVoto.java
@@ -0,0 +1,16 @@
+package atividades;
+
+public class VerificadorVoto {
+    private int idade;
+    
+    public VerificadorVoto(int idade){
+        this.idade = idade;
+}
+    public void podeVotar(){
+        if (idade >= 16){
+            System.out.println("Pode votar");
+        } else {
+            System.out.println("Nao pode votar");
+        }
+    }
+}
